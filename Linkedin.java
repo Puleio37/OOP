a8ddaa4969706bc5b0f495da4d1a8e78ee1bbd06
Puleio37 <40067251+Puleio37@users.noreply.github.com>
2020-12-29 15:27:23 -0500

    Delete Linkedin.java

--- Linkedin.java
+++
@@ -1,8 +0,0 @@
-
-public class Linkedin implements SocialMedia{//Linkedin class that implements SocialMedia
-	
-	public void useMedia() {//this methods allows for whatever accounts call it to be using Linkedin
-		System.out.println(" is using Linkedin");
-	}
-
-}
