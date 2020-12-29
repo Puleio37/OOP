3ad3c72bec62e5d9256ddfbca26b99930567b2e5
Puleio37 <40067251+Puleio37@users.noreply.github.com>
2020-12-29 15:26:58 -0500

    Delete SocialMedia.java

--- SocialMedia.java
+++
@@ -1,6 +0,0 @@
-
-public interface SocialMedia {//Social media interface implementing all social media classes
-	
-	void useMedia();//interface method used in media classes in order accounts to use that media
-
-}
