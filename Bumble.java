346ae032e7fdd461c360a614a21a31a557f480b8
Puleio37 <40067251+Puleio37@users.noreply.github.com>
2020-12-29 15:27:59 -0500

    Delete Bumble.java

--- Bumble.java
+++
@@ -1,9 +0,0 @@
-
-public class Bumble implements SocialMedia{//Bumble class that is implementing SocialMedia
-	
-	public void useMedia() {//this methods allows for whatever accounts calls it to be using bumble
-		System.out.println(" is using Bumble");
-	}
-	
-
-}
